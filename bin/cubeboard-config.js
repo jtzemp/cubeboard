63d2bc7dc1fbcf398ee1d6f2090a0432d98fd981
Jason Lauman <jason@lauman.net>
2012-11-27 17:20:31 -0700

    Replaced WebSocket with http-polling in visualizer.

--- bin/cubeboard-config.js
+++ bin/cubeboard-config.js
@@ -3,7 +3,7 @@ module.exports = {
   "mongo-host": "127.0.0.1",
   "mongo-port": 27017,
   "mongo-database": "cube_development",
-  "evaluator-http-host": '127.0.0.1',
+  "evaluator-http-host": '10.3.100.154',
   "evaluator-http-port": 1081,
   "cubeboard-http-port": 1082
 };
